4a9aa0babbbe2bb4b4715d6b9b4f862180e35a6e
Madhav2246 <yalamarthi.sriram123@gmail.com>
2026-05-06 16:17:02 +0530

    final commit

--- notification_app_be/server.js
+++ notification_app_be/server.js
@@ -2,7 +2,13 @@ require("dotenv").config();
 
 const express = require("express");
 
+const cors = require("cors");
+
 const app = express();
+
+
+// ENABLE CORS
+app.use(cors());
 
 app.use(express.json());
 

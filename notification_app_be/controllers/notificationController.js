b8ab85c5241e0f18bce9c0110976cae545aa7fb8
Madhav... <143095911+Madhav2246@users.noreply.github.com>
2026-05-07 10:26:43 +0530

    Refactor notification controller for consistency

--- notification_app_be/controllers/notificationController.js
+++ notification_app_be/controllers/notificationController.js
@@ -1,71 +1,31 @@
 const notifications = require("../data/notifications");
-
 const Log = require("../../logging_middleware/logger");
 
-
 // GET ALL NOTIFICATIONS
-const getNotifications = async (req, res) => {
-
-    await Log(
-        "backend",
-        "info",
-        "controller",
-        "Fetched all notifications"
-    );
-
+const getNotifications = async(req, res) => {
+    await Log("backend", "info", "controller", "Fetched all notifications.");
     res.json(notifications);
 };
 
-
-
 // CREATE NEW NOTIFICATION
-const createNotification = async (req, res) => {
-
+const createNotification = async(req, res) => {
     const newNotification = {
-        id: notifications.length + 1,
+        id: notifications.length+1,
         type: req.body.type,
         message: req.body.message,
         priority: req.body.priority,
         read: false
     };
-
     notifications.push(newNotification);
-
-    await Log(
-        "backend",
-        "info",
-        "controller",
-        "Created new notification"
-    );
-
-    res.json({
-        success: true,
-        notification: newNotification
-    });
+    await Log("backend", "info", "controller", "Created new notification");
+    res.json({success: true, notification: newNotification});
 };
 
-
-
 // GET PRIORITY NOTIFICATIONS
-const getPriorityNotifications = async (req, res) => {
-
-    const sortedNotifications = notifications.sort(
-        (a, b) => b.priority - a.priority
-    );
-
-    await Log(
-        "backend",
-        "info",
-        "controller",
-        "Fetched priority notifications"
-    );
-
+const getPriorityNotifications = async(req, res) => {
+    const sortedNotifications = notifications.sort((a, b) => b.priority-a.priority);
+    await Log("backend", "info", "controller", "Fetched priority notifications");
     res.json(sortedNotifications);
 };
 
-
-module.exports = {
-    getNotifications,
-    createNotification,
-    getPriorityNotifications
-};
+module.exports = {getNotifications, createNotification, getPriorityNotifications};

f2e5269d3bc08653fba2c3c6a28e80338780a54b
Madhav... <143095911+Madhav2246@users.noreply.github.com>
2026-05-07 10:29:39 +0530

    Refactor server.js for cleaner code structure

--- notification_app_be/server.js
+++ notification_app_be/server.js
@@ -1,30 +1,17 @@
 require("dotenv").config();
-
 const express = require("express");
-
 const cors = require("cors");
-
 const app = express();
 
-
-// ENABLE CORS
 app.use(cors());
-
 app.use(express.json());
 
-
-// IMPORT ROUTES
 const notificationRoutes = require("./routes/notificationRoutes");
-
-
-// USE ROUTES
 app.use("/notifications", notificationRoutes);
 
-
-app.get("/", (req, res) => {
+app.get("/", (req,res) => {
     res.send("Notification Backend Running");
 });
-
 
 app.listen(3000, () => {
     console.log("Server Started");

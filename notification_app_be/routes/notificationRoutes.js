a4970b9c068aa21f01e4b73a586f5f27d6f5fdee
Madhav... <143095911+Madhav2246@users.noreply.github.com>
2026-05-07 10:28:12 +0530

    Update notificationRoutes.js

--- notification_app_be/routes/notificationRoutes.js
+++ notification_app_be/routes/notificationRoutes.js
@@ -1,21 +1,7 @@
 const express = require("express");
-
 const router = express.Router();
-
-const {
-    getNotifications,
-    createNotification,
-    getPriorityNotifications
-} = require("../controllers/notificationController");
-
-
-
+const {getNotifications, createNotification, getPriorityNotifications} = require("../controllers/notificationController");
 router.get("/", getNotifications);
-
 router.post("/", createNotification);
-
 router.get("/priority", getPriorityNotifications);
-
-
-
 module.exports = router;
